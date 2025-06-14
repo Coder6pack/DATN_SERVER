7712dc80c2f048d7bba6f92db92d4885b8412acc
Coder6pack <hoainhonlam10@gmail.com>
2025-06-14 14:53:44 +0700

    handle ecosystem and pck.json

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,7 @@ module.exports = {
 	apps: [
 		{
 			name: 'shopping',
-			script: 'dist/src/main.js',
-			env: {
-				NODE_ENV: 'development', // Riêng NODE_ENV thì có thể dùng process.env.NODE_ENV hoặc process.NODE_ENV, còn lại thì chỉ được dùng process.env.TEN_BIEN
-				TEN_BIEN: 'Gia tri',
-			},
-			env_production: {
-				NODE_ENV: 'production',
-			},
+			script: 'npm run start:prod',
 		},
 	],
 }
